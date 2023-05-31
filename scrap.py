47dfe84878e74e4a9b3bebf3e77777243ed8e8ff
KhTalhaArshad <126275156+KhTalhaArshad@users.noreply.github.com>
2023-05-31 09:55:37 +0500

    Update scrap.py

--- scrap.py
+++ scrap.py
@@ -4,7 +4,5 @@ import requests
 #the website URL
 url_link = "https://en.wikipedia.org/wiki/List_of_states_and_territories_of_the_United_States"
 result = requests.get(url_link)
-  .text
 doc = BeautifulSoup(result, "html.parser")
-
 print(doc.prettify())
